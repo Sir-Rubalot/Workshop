dd5c899622173dcdb2e25d414b97e9e108192f1c
Sir-Rubalot <robinekbladh@hotmail.com>
2025-10-28 15:22:01 +0100

    Mini-community

--- Community.py
+++ Community.py
@@ -1,47 +1,50 @@
 community = []
 
-def add_user(community):
-    add_user = input("New username: ")
+def add_user():
+    username = input("New username: ").strip()
+    password = input("New password: ").strip()
     if add_user == "":
         print("Please enter a valid username")
-    else:
-        community.append(add_user)
-        print("User created!")
-
-def log_in(community):
-    log_in = input("Enter username: ")
-    if log_in == add_user:
-        print("Enter your password: ")
-
-def remove_user(community):
-    if len(community) == 0:
-        print("playlist is empty")
+        return
+    elif any(user['username'] == username for user in community):
+        print("Username already taken, dummy!")
         return
     
-    show_members(community)
-    
-    print("---DevOps community---")
-    for i, song in enumerate(community, start=1):
-        print(f"{i}: {song}")
+    community.append({'username': username, 'password': password})
+    print("User created!")
 
-    user_to_remove = (input("Which user do you want to remove?: ")).strip()
-    if not user_to_remove.isdigit():
-        print("Not a number!!!")
+def log_in():
+    username = input("Enter username: ").strip()
+    password = input("Enter password: ").strip()
 
-    user_number = int(user_to_remove)
-    if user_number < 1 or user_number >len(community):
-        print("Not in range of playlist, idiot")
+    for user in community:
+        if user['username'] == username and user['password'] == password:
+            print("Log in successful!")
+            return
+        print("username or password incorrect")
 
-    remove_song = community.pop(user_number - 1)
-    print (f"success! Removed user {remove_user}")
-
-def show_members(community):
-    if len(community) == 0:
+def remove_user():
+    if not community:
         print("Community is empty")
+        return
+    
+    show_members()
+    
+    user_to_remove = input("Which user do you want to remove?: ").strip()
+    for i, user, in enumerate(community):
+        if user ['username'] == user_to_remove:
+            community.pop(i)
+            print(f"Successfully removed {user_to_remove}")
+            return
+
+def show_members():
+    if not community:
+        print("Community is empty")
+        return
 
     print("---DevOps Community---")
-    for i, song in enumerate(community, start=1):
-        print(f"{i}: {song}")
+    for i, user in enumerate(community, start=1):
+        print(f"{i}: {user['username']}")
 
 def main():
     while True:
@@ -54,16 +57,15 @@ def main():
         user_choice = input(">: ").strip()
 
         if user_choice == "1":
-            add_user(community)
+            add_user()
         elif user_choice == "2":
-            log_in(community)
+            log_in()
         elif user_choice == "3":
-            remove_user(community)
+            remove_user()
         elif user_choice == "4":
-            show_members(community)
+            show_members()
         elif user_choice == "0":
             break
         else:
             print("Invalid choice")
-
 main()
